897bcef590abbe2fb2533e393427f08e46f3b1ff
admin <127>
2018-09-09 12:11:05 +0800

    第一次提交代码到git仓库

---
+++ crm-domain/src/main/java/cn/sm1234/domain/Customer.java
@@ -0,0 +1,48 @@
+package cn.sm1234.domain;
+
+/**
+ * @author http://www.sm1234.cn
+ * @version 1.0
+ * @ description
+ * cn.sm1234.domain
+ * @date 2018/9/7
+ */
+public class Customer{
+
+    private Integer id;
+    private String name;
+    private String gender;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ crm-web/src/main/webapp/WEB-INF/jsp/succ.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2018/9/8
+  Time: 22:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>提示</title>
+</head>
+<body>
+   保存成功
+</body>
+</html>
